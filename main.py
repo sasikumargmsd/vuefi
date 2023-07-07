37e537a60ae9640e48b4c2b4bca41f00a33633b1
Sasikumar Ganesan <sasikumarganesan@SasikumarGanesan-MacBook-Pro.local>
2023-07-07 23:32:14 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import os 
+os.environ["OPENAI_API_KEY"] = "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"
+
+import json
+from langchain.document_loaders import PyPDFLoader
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+@app.route('/check_eligibility', methods=['POST'])
+def create_item():
+	record = json.loads(request.data)
+	loader = PyPDFLoader(record['url'])
+	documents = loader.load()
+	index = VectorstoreIndexCreator().from_loaders([loader])
+	query = "what is the cibil score?"
+	score = index.query(query)
+	query = "enquiries in last 1 month?"
+	enquiries = index.query(query)
+	return jsonify({"score": score.strip(), "enquiries":enquiries.strip()})
+
+if __name__ == '__main__': 
+	app.run()
